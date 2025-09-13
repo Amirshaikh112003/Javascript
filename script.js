b9d5e30785e21a0543020c2eb2b22e2af9dc8d89
Amir Shaikh <your-email@example.com>
2025-09-13 15:53:51 +0530

    javascript_dom

---
+++ script.js
@@ -0,0 +1,19 @@
+// let h1= document.querySelector("h1");
+// console.dir(h1);
+// h1.innerHTML = "h";
+// // let a = document.querySelector("a");
+// // a.href = "https://www.google.com";
+//
+// //setattribute
+// let a  = document.querySelector("a");
+// a.setAttribute("href", "https://www.google.com");
+//getAttribute
+// console.log(a.getAttribute("href"));
+// removeAttribute
+// a.removeAttribute("href");
+
+//js se css badalna
+
+let h1 = document.querySelector("h1");
+h1.style.color = "red";
+h1.style.backgroundColor = "orange";
