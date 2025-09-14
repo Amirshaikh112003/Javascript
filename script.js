29349c835fbe66ff89d80ce9f54bedc9ddbaee36
Amir Shaikh <your-email@example.com>
2025-09-14 23:34:39 +0530

    event and event listeners

--- script.js
+++ script.js
@@ -14,7 +14,45 @@
 
 //js se css badalna
 
-let h1 = document.querySelector("h1");
-h1.style.color = "red";
-h1.style.backgroundColor = "orange";
+// let h1 = document.querySelector("h1");
+// h1.style.color = "red";
+// h1.style.backgroundColor = "orange";
+
+// Event and Event listeners
+
+//input eventlistener
+// let inp = document.querySelector("input");
+// inp.addEventListener("input",(e)=>{
+//     if(e.data !== null) {
+//         console.log(e.data);
+//     }
+// });
+
+//click event
+
+// let click = document.querySelector("div");
+// function a(){
+//     click.style.color = "red";
+// }
+// click.addEventListener("click",a);
+
+//change event
+
+// let sel = document.querySelector('select');
+// let dev = document.querySelector('#device');
+// sel.addEventListener('change', e => {
+//     dev.textContent = `${e.target.value} selected`;
+// });
+
+//keydown
+
+// let h1 = document.querySelector('h1');
+// window.addEventListener('keydown', (e) => {
+//     if(e.key === " ") {
+//         h1.textContent = "SPC"
+//     }
+//     else{
+//         h1.textContent = e.key;
+//     }
+//});
 
